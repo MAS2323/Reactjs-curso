fe143ac71038d7bccfcb7d2a1559fd86f360116e
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2025-03-31 22:15:49 +0800

    segundo commit

--- proyect/00-hola-mundo/src/App.jsx
+++ proyect/00-hola-mundo/src/App.jsx
@@ -3,24 +3,47 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
   //   const mo = {isFollowing: true, userName: 'Mas Onewe'}
   //   const maso = {isFollowing: true, userName: 'Mas Onewe'}
+
+  const users = [
+    {
+      userName: "MasO",
+      name: "Mas Onewe",
+      isFollowing: true,
+    },
+    {
+      userName: "Onewe",
+      name: "Vicencio",
+      isFollowing: false,
+    },
+    {
+      userName: "23OM",
+      name: "Consuelo",
+      isFollowing: true,
+    },
+    {
+      userName: "Comasa",
+      name: "Remigio",
+      isFollowing: true,
+    },
+    {
+      userName: "Osma",
+      name: "Samuel",
+      isFollowing: false,
+    },
+  ];
+
   return (
     <section className="App">
-      <TwitterFollowCard
-        initialisFollowing={true}
-        userName="MasO"
-        name="Mas Onewe"
-      />
-      <TwitterFollowCard
-        isFollowing={false}
-        userName="elonmusk"
-        name="Vicencio Onewe"
-      />
-      <TwitterFollowCard
-        isFollowing={false}
-        userName="vxm"
-        name="Victoriano Mas"
-      />
-      <TwitterFollowCard isFollowing userName="sam" name="Samuel Mas" />
+      {users.map(({ userName, name, isFollowing }) => {
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialisFollowing={isFollowing}
+            name={name}
+          />
+        );
+      })}
     </section>
   );
 }
